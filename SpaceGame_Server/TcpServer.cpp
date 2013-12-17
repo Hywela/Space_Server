962c3cc4f4e10da1d4d9ccc5c426962a7da1b1fb
Hywela <Kriss_kb@hotmail.com>
2013-12-17 19:56:15 +0100

    ada

--- SpaceGame_Server/TcpServer.cpp
+++ SpaceGame_Server/TcpServer.cpp
@@ -4,7 +4,7 @@
 TcpServer::TcpServer(){
 	string serverName = "25.219.167.39";
 	db = new MySqlHandler();
-    db->set_database_info("mysql.stud.hig.no","imt3601h13gr01",3306,"imjRWuq2");
+    db->set_database_info(;
     db->init_connection();
 	receivedByteCount=0;
 	clientCount = 0;
